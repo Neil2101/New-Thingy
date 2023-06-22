32b8744622b3b4059f507c08227e988943af46b5
Neil2101 <66876298+Neil2101@users.noreply.github.com>
2023-06-21 21:44:30 -0600

    Day 3
    Part 2, I got bored LOL

--- Characters/playerchar.cs
+++ Characters/playerchar.cs
@@ -6,14 +6,14 @@ public partial class playerchar : CharacterBody2D
 {
 	[Export]
 	public int Speed { get; set; } = 50;
-	public int Gravity { get; set; } = 1;
+	public int Gravity { get; set; } = 10;
 	public float yvel = 0;
-	public touching_ground = false;
 	public void GetInput()
 	{
 		Vector2 yv_vector = new Vector2(0,yvel);
-		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-		Velocity = (inputDirection * Speed) - yv_vector; //<-- the direction vector multiplied by speed - down velocity
+		Vector2 inputDirection = Input.GetVector("right", "left", "up","down");//improvised code, hope null works -Minty
+		Vector2 jump = Input.GetVector(null,null,"jump",null);
+		Velocity = (inputDirection * Speed) + yv_vector; //<-- the direction vector multiplied by speed + down velocity
 		yvel -= Gravity;
 	}
 
@@ -22,16 +22,4 @@ public partial class playerchar : CharacterBody2D
 		GetInput();
 		MoveAndSlide();
 	}
-}
-
-
-private void _on_area_2d_body_entered(Node2D body)
-{
-	touching_ground = true;
-}
-
-
-private void _on_area_2d_area_shape_exited(Rid area_rid, Area2D area, long area_shape_index, long local_shape_index)
-{
-	touching_ground = false;
 }
