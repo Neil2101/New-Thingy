bfc6a72cfaa58d5e3d01c7236ef193e1e9dde59d
Neil2101 <66876298+Neil2101@users.noreply.github.com>
2024-05-28 17:28:22 -0400

    Delete Characters directory

--- Characters/playerchar.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-
-
-public partial class playerchar : CharacterBody2D
-{
-	//What are you looking for here? This is the wrong playerchar.
-}
--- Characters/potato gun.gd
+++
@@ -1,5 +0,0 @@
-"""
-What are you doing here, digging around? Don't be surprised when you find what you don't want to find, hear what you don't want to hear.
-
--Ominous message by dev Mintysharky, known as Neil in the comments where he is very frustrated and angry and wants to go punch a walll
-"""
